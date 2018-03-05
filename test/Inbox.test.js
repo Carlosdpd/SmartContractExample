f8898cd59f1e25dc02cda60a09136ee91500729b
Carlos Pires <krlos.pires@gmail.com>
2018-03-05 14:16:14 -0400

    Added deploy to rinkedby network

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,23 +2,45 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 //Instance
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
+//Gettin interface and bytecode from the compile file
+const { interface, bytecode} = require ('../compile');
 
-beforeEach(() => {
+let accounts;
+let inbox;
+const INITIAL_STRING = 'Hi There!';
+const NEW_STRING = 'Bye';
+
+beforeEach( async () => {
 
   //Get a list of all accounts
-  web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts);
-    });
+  accounts = await web3.eth.getAccounts();
   //Use one of the accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .send( {from: accounts[0], gas: '1000000'} );
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-
+      assert.ok(inbox.options.address);
   });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it('can change the message', async() => {
+    await inbox.methods.setMessage(NEW_STRING).send( {from: accounts[0]} );
+    const message = await inbox.methods.message().call();
+    assert.equal(message, NEW_STRING);
+  });
+
 });
 
 
